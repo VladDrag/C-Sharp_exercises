4677bb6dd4eb38a30e33a843866b9dc801ba039a
VladDrag <vdragomir@protonmail.com>
2022-05-11 09:20:05 +0200

    added ex02

---
+++ ex02/ReferenceEx/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+void changeB(ref int b)
+{
+	b++;
+}
+int multiply(int a)
+{
+	var b = 0;
+	changeB(ref b);
+	return a * b;
+}
+
+int min()
+{
+	Console.WriteLine(multiply(3));
+	return 0;
+}
+
+min();
+ 
