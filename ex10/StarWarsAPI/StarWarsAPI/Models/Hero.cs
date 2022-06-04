29246ccd6319c8f19a6a3fdc2ec83e3f63824b57
VladDrag <vdragomir@protonmail.com>
2022-06-04 21:14:07 +0200

    Added Models for SWAPI

--- ex10/StarWarsAPI/StarWarsAPI/Models/Hero.cs
+++ ex10/StarWarsAPI/StarWarsAPI/Models/Hero.cs
@@ -5,6 +5,23 @@ namespace StarWarsAPI.Models
     public class Hero
     {
         [JsonPropertyName("name")]
-        public string 
+        public string Name { get; }
+        
+        [JsonPropertyName("gender")]
+        public string Gender { get; }
+        
+        [JsonPropertyName("height")]
+        public string HeightInCm { get; }
+
+        public string HeightInMm
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(HeightInCm)) return "0";
+                if (!int.TryParse(HeightInCm, out var result)) return "0";
+
+                return (result * 10).ToString();
+            }
+        }
     }
 }
