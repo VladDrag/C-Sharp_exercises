31eff83a80206ce619804b3ff340efe3d8113d90
eirik lysenstoen <elysenstoen@gmail.com>
2022-06-04 22:34:40 +0200

    writing lines with failure

--- ex10/StarWarsAPI/StarWarsAPI/Program.cs
+++ ex10/StarWarsAPI/StarWarsAPI/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using StarWarsAPI.Controllers;
+using StarWarsAPI.Services;
 
 namespace StarWarsAPI
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var service = new HeroesDownloader();
+            var controller = new SwapiController(service);
+
+            var heroes = await controller.HeroesCreator();
+            var toptext = $"There are {heroes.NumberOfHeroes} heroes in this document";
+            
+            // Write the number of heroes heroes.numberof then the name of each hero
+            var namelist = heroes.HeroList.Select(hero => hero.Name);
+            namelist.Prepend(toptext);
+            await File.WriteAllLinesAsync("WriteLines.txt", namelist);
+
         }
     }
 }
