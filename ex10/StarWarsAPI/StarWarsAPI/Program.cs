e1b2ae81227ed7752cd6598502c42d1ffc1a423f
VladDrag <vdragomir@protonmail.com>
2022-06-05 17:46:52 +0200

    added argument functionality for all heroes to be printed to the Result.txt

--- ex10/StarWarsAPI/StarWarsAPI/Program.cs
+++ ex10/StarWarsAPI/StarWarsAPI/Program.cs
@@ -10,25 +10,33 @@ namespace StarWarsAPI
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static async Task PrintAllHeroes()
         {
             var service = new HeroesDownloader();
             var controller = new SwapiController(service);
 
-            // var heroesResponse = await controller.HeroesCreator();
-            // var heroes = heroesResponse.HeroList;
+            var heroesResponse = await controller.HeroesCreator();
+            var heroes = heroesResponse.HeroList;
+            
+            File.WriteAllText("Result.txt", "Count is = " + heroesResponse.NumberOfHeroes + "\n");
+            foreach (var hero in heroes)
+            {
+                File.AppendAllText("Result.txt", "Hero name is :" + hero.Name + "\n");
+            }
+        }
+        // "all" / "id=1"
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+            if (args[0] == "all") await PrintAllHeroes();
+            // var service = new HeroesDownloader();
+            // var controller = new SwapiController(service);
             //
-            // File.WriteAllText("Result.txt", "Count is = " + heroes.Count + "\n");
-            // foreach (var hero in heroes)
-            // {
-            //     File.AppendAllText("Result.txt", "Hero name is :" + hero.Name + "\n");
-            // }
-
-            var id = "1";
-
-            var heroResponse = await controller.HeroCreator(id);
-            File.WriteAllText("Result.txt",$"{heroResponse.Name} is of the gender {heroResponse.Gender}, " +
-                                           $"and is {heroResponse.HeightInCm} centimeters tall.");
+            // var id = "1";
+            //
+            // var heroResponse = await controller.HeroCreator(id);
+            // File.WriteAllText("Result.txt",$"{heroResponse.Name} is of the gender {heroResponse.Gender}, " +
+            //                                $"and is {heroResponse.HeightInCm} centimeters tall.");
 
         }
     }
